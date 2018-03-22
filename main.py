e92b139565dbb866cd6609573e129e01183848b6
HaoTse <top30339@gmail.com>
2018-03-22 14:22:50 +0800

    Complete computing query result

--- main.py
+++ main.py
@@ -32,6 +32,25 @@ def load_csv(file_name):
     df = pd.read_csv(file_name, delimiter=',', header=None)
     return df
 
+def check(ty, querys, row):
+    if ty == 'and':
+        for query in querys:
+            if not query.strip() in row[1]:
+                return False
+        return True
+    elif ty == 'or':
+        for query in querys:
+            if query.strip() in row[1]:
+                return True
+        return False
+    elif ty == 'not':
+        for query in querys:
+            if query.strip() in row[1]:
+                return False
+        return True
+    
+    return False
+
 if __name__ == '__main__':
     import argparse
 
@@ -51,10 +70,42 @@ if __name__ == '__main__':
     
     # TODO load source data, build search engine
     source_data = load_csv(args.source)
-    source_data.iloc[:, 1] = source_data.apply(cut, axis=1)
-    print(source_data)
+    # source_data.iloc[:, 1] = source_data.apply(cut, axis=1)
     print('Finish loading source data, and building search engine.')
 
     # TODO compute query result
+    # read query file
+    with open(args.query, 'r', encoding='utf-8') as f:
+        lines = f.readlines()
+    # compute query result
+    outputs = []
+    for line in lines:
+        # get querys type and content
+        if 'and' in line:
+            ty = 'and'
+            querys = line.split('and')
+        elif 'or' in line:
+            ty = 'or'
+            querys = line.split('or')
+        elif 'not' in line:
+            ty = 'not'
+            querys = line.split('not')
+        else:
+            print(line + ': format error!')
+            continue
+        
+        # compute
+        result = []
+        for index, row in source_data.iterrows():
+            if check(ty, querys, row):
+                result.append(str(row[0]))
+
+        # check if has result
+        if result:
+            outputs.append(result)
+        else:
+            outputs.append([str(0)])
   
     # TODO output result
+    for output in outputs:
+        print(','.join(output))
